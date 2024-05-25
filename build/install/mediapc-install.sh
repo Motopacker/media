3cc942080e854a56adc41f1aeecd3a8673499627
XRPilot <brogan.lowe@gmail.com>
2024-05-25 17:39:45 +1000

    Fixed username variables

--- build/install/mediapc-install.sh
+++ build/install/mediapc-install.sh
@@ -26,12 +26,12 @@ $STD apt-get -y install {va-driver-all,ocl-icd-libopencl1,intel-opencl-icd,vainf
 
 if [[ "$CTTYPE" == "1" ]]; then
  $STD groupadd -g 11000 lxc_gpu_shares
- $STD gpasswd -a "$USER_NAME" lxc_gpu_shares
+ $STD gpasswd -a "$USERNAME" lxc_gpu_shares
 fi
 
 msg_info "Creating user account"
-$STD useradd -u 1000 -m -s /usr/bin/bash "$USER_NAME"
-sudo adduser "$USER_NAME" sudo
+$STD useradd -u 1000 -m -s /usr/bin/bash "$USERNAME"
+sudo adduser "$USERNAME" sudo
 
 msg_info "Tweak VM for performance"
 $STD "{ vm.swappiness=10; vm.vfs_cache_pressure = 50; fs.inotify.max_user_watches=262144 }" >> /etc/systemctl.conf
@@ -51,24 +51,24 @@ msg_info "Installing Docker and Docker Compose"
 $STD bash -c "$(curl -fsSL https://get.docker.com -o get-docker.sh)"
 
 msg_info "Adding user account to docker group"
-$STD sudo adduser "$USER_NAME" docker
+$STD sudo adduser "$USERNAME" docker
 
 msg_info "Creating Docker directories if they don't exist and setting permissions"
 #if [ ! -d /home/mlzboy/b2c2/shared/db ]; then
 #  mkdir -p /home/mlzboy/b2c2/shared/db;
 #fi
 
-$STD mkdir -p /home/"$USER_NAME"/docker/appdata /home/"$USER_NAME"/docker/compose /home/"$USER_NAME"/docker/logs /home/"$USER_NAME"/docker/scripts /home/"$USER_NAME"/docker/secrets /home/"$USER_NAME"/docker/shared
-$STD sudo chown root:root /home/"$USER_NAME"/docker/secrets
-$STD sudo chmod 600 /home/"$USER_NAME"/docker/secrets
-$STD sudo setfacl -Rdm u:xrpilot:rwx /home/"$USER_NAME"/docker
-$STD sudo setfacl -Rm u:xrpilot:rwx /home/"$USER_NAME"/docker
-$STD sudo setfacl -Rdm g:docker:rwx /home/"$USER_NAME"/docker
-$STD sudo setfacl -Rm g:docker:rwx /home/"$USER_NAME"/docker
+$STD mkdir -p /home/"$USERNAME"/docker/appdata /home/"$USERNAME"/docker/compose /home/"$USERNAME"/docker/logs /home/"$USERNAME"/docker/scripts /home/"$USERNAME"/docker/secrets /home/"$USERNAME"/docker/shared
+$STD sudo chown root:root /home/"$USERNAME"/docker/secrets
+$STD sudo chmod 600 /home/"$USERNAME"/docker/secrets
+$STD sudo setfacl -Rdm u:xrpilot:rwx /home/"$USERNAME"/docker
+$STD sudo setfacl -Rm u:xrpilot:rwx /home/"$USERNAME"/docker
+$STD sudo setfacl -Rdm g:docker:rwx /home/"$USERNAME"/docker
+$STD sudo setfacl -Rm g:docker:rwx /home/"$USERNAME"/docker
 
-$STD touch /home/"$USER_NAME"/docker/.env
-$STD sudo chown root:root /home/"$USER_NAME"/docker/.env
-$STD sudo chown 600 /home/"$USER_NAME"/docker/.env
+$STD touch /home/"$USERNAME"/docker/.env
+$STD sudo chown root:root /home/"$USERNAME"/docker/.env
+$STD sudo chown 600 /home/"$USERNAME"/docker/.env
 
 motd_ssh
 customize
