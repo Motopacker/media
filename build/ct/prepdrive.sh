a1d99aa1af4689e99408afffbab182176d0fa510
XRPilot <brogan.lowe@gmail.com>
2024-05-25 21:14:32 +1000

    Remove subuid + subgid from build

--- build/ct/prepdrive.sh
+++ build/ct/prepdrive.sh
@@ -1,25 +1,33 @@
+
+bash -c "$(wget -qLO - https://github.com/Motopacker/media/raw/main/build/ct/mediapc.sh)"
+cat /etc/pve/lxc/101.conf
+nano /etc/pve/lxc/101.conf
+
+
+#Run against host:
     mkdir -p /mnt/docker /mnt/data
     chown 1000:1000 /mnt/docker -R
     chown 1000:1000 /mnt/data -R  
 
+grep -qxF 'include "/etc/subuid"' root:1000:1 || echo 'include "/etc/subuid"' >> root:1000:1
+grep -qxF 'include "/etc/subgid"' root:1000:1 || echo 'include "/etc/subgid"' >> root:1000:1  
 
+# Run against LXC:
 msg_info "Creating user account"
 $STD useradd -u 1000 -m -s /usr/bin/bash "$USERNAME"
 sudo adduser "$USERNAME" sudo
-
-
  $STD groupadd -g 11000 lxc_gpu_shares
  $STD gpasswd -a "$USERNAME" lxc_gpu_shares
-fi
 
-#Add maps to LXC config
+
+#Add maps to LXC config. Run against host:
 Ask for container ID
-
 cat <<EOF >>/etc/pve/lxc/$LXC_ID.conf
 lxc.mount.entry: /mnt/docker mnt/home/$USER_NAME/docker none bind 0 0
 lxc.mount.entry: /mnt/data data none bind 0 0
 EOF
 
+#Run on LXC
 msg_info "Adding user account to docker group"
 $STD sudo adduser "$USERNAME" docker
 
